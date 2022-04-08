637229a415e186dac81bd6a8d535c6ecb5ab2301
tkr-code <malick.tounkara.1@gmail.com>
2022-04-08 20:32:04 +0000

    Messagerie: 41%

--- src/Controller/Admin/ProfileController.php
+++ src/Controller/Admin/ProfileController.php
@@ -4,8 +4,8 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use App\Entity\Phone;
-use App\Form\ProfileType;
 use App\Form\PhoneType;
+use App\Form\ProfileType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
